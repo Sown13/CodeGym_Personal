edadaa7eef23ae76b6e2887064654e0ab87123df
Sown13 <sonnh.buh@gmail.com>
2023-02-23 14:05:20 +0700

    JS Output practice

--- JavaScript/Practice_1_Output/output.js
+++ JavaScript/Practice_1_Output/output.js
@@ -1,3 +1,9 @@
 function showMessage(){
     alert('test xem có lên không')
 }
+function showMessage2(){
+    document.write("lên thật này")
+}
+function showMessage3(){
+    console.log("ok rồi thoát đi");
+}

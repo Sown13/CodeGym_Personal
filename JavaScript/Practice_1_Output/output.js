6e805181921116641c2b3219509bdfc74499c22e
Sown13 <sonnh.buh@gmail.com>
2023-02-23 15:40:40 +0700

    mouseover mouseout

--- JavaScript/Practice_1_Output/output.js
+++ JavaScript/Practice_1_Output/output.js
@@ -7,3 +7,15 @@ function showMessage2(){
 function showMessage3(){
     console.log("ok rồi thoát đi");
 }
+let mouse1 = document.getElementById("mouse1")
+mouse1.addEventListener("mouseover",mouseover1)
+mouse1.addEventListener("mouseout",mouseout1)
+function mouseover1(){
+    document.getElementById("mouse1").style.color= "red";
+    document.getElementById("mouse2").style.color= "blue";
+}
+function mouseout1(){
+    document.getElementById("mouse1").style.color = "black";
+    document.getElementById("mouse2").style.color = "red";
+}
+

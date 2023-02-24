9231da846495f41fd1785cbb2d72919c5ec6d0a6
Sown13 <sonnh.buh@gmail.com>
2023-02-24 11:02:07 +0700

    Practice Var decleration

---
+++ JavaScript/Practice_2_Operator/var_decleration.js
@@ -0,0 +1,21 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = "Hồ Chí Minh"
+let firstsen = "kết quả của toán tử"
+let secondsen = "được in ra"
+let thirdsen = "nhưng bị viết liền"
+let forthsen = "muốn in ra có dấu cách"
+let fifthsen = "thì toán tử thêm cả dấu cách vào"
+
+document.write("i =" + i)
+document.write("<br>")
+document.write("f bằng" + " " + f)
+document.write("<br>")
+document.write("b =" + b)
+document.write("<br>")
+document.write("s =" + s)
+document.write("<br>")
+document.write(firstsen + secondsen + thirdsen)
+document.write("<br>")
+document.write(forthsen + " " + fifthsen)

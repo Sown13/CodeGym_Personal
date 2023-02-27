7126d89c6d95e2d8e0d0ad98ba888d7da3b88fca
Sown13 <sonnh.buh@gmail.com>
2023-02-27 09:24:08 +0700

    money converse done

--- JavaScript/Practice_2_Operator/Assaignment/money_converse.js
+++ JavaScript/Practice_2_Operator/Assaignment/money_converse.js
@@ -6,11 +6,17 @@
 function currencyConverse(){
     let a,b,c,d;
     a = document.getElementById("currency").valueAsNumber;
-    b = document.getElementById("fromNation").valueAsNumber;
-    c = document.getElementById("toNation").valueAsNumber;
+    console.log(a)
+    b = parseInt(document.getElementById("fromNation").value);
+    console.log(b)
+    c = parseInt(document.getElementById("toNation").value);
+    console.log(c)
     d = a*(c/b)
     document.getElementById("result").innerHTML = d
+
+
 }
+
 
 document.getElementById("converse").addEventListener("click",currencyConverse);
 // document.getElementById("fromNation").addEventListener("onchange",getValue());
